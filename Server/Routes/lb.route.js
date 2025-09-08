b03f2a4301e35ee5254468d5e64eae1caa908544
sabarim6369 <sabarim6369@gmail.com>
2025-09-09 00:10:02 +0530

    updated lb routes

--- Server/Routes/lb.route.js
+++ Server/Routes/lb.route.js
@@ -1,21 +1,36 @@
 import { Hono } from "hono";
 import {
-  createLB, listLBs, getLB, addInstance, removeInstance,
-  proxyRequest, getMetrics
+  createLB,
+  listLBs,
+  getLB,
+  updateLB,
+  deleteLB,
+  addInstance,
+  updateInstance,
+  removeInstance,
+  proxyRequest,
+  getMetrics
 } from "../Controllers/lbController.js";
 import { authMiddleware } from "../Middleware/authMiddleware.js";
 
 const router = new Hono();
 
-// protected LB management
+// LB routes
 router.get("/", authMiddleware, listLBs);
 router.post("/", authMiddleware, createLB);
 router.get("/:lbId", authMiddleware, getLB);
+router.put("/:lbId", authMiddleware, updateLB);
+router.delete("/:lbId", authMiddleware, deleteLB);
+
+// Instance routes
 router.post("/:lbId/instances", authMiddleware, addInstance);
+router.put("/:lbId/instances", authMiddleware, updateInstance);
 router.delete("/:lbId/instances", authMiddleware, removeInstance);
+
+// Metrics
 router.get("/:lbId/metrics", authMiddleware, getMetrics);
 
-// public proxy endpoint (you might protect it differently)
+// Public proxy endpoint
 router.all("/proxy/:lbId/*", proxyRequest);
 
 export default router;
