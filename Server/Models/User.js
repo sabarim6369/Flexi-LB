e666d6218660dd5e4a4b75ab3176538bf97acaf8
sabarim6369 <sabarim6369@gmail.com>
2025-09-09 23:03:26 +0530

    Working on settings page

--- Server/Models/User.js
+++ Server/Models/User.js
@@ -3,7 +3,13 @@ import mongoose from "mongoose";
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email:    { type: String, required: true, unique: true },
-  password: { type: String, required: true }, // hashed
+  password: { type: String, required: true },
+    notifications: {
+    emailAlerts: { type: Boolean, default: true },
+    serverDownAlerts: { type: Boolean, default: true },
+    performanceAlerts: { type: Boolean, default: true },
+    weeklyReports: { type: Boolean, default: false },
+  },
 }, { timestamps: true });
 
 export default mongoose.models.User || mongoose.model("User", UserSchema);
--- Server/Routes/auth.route.js
+++ Server/Routes/auth.route.js
@@ -1,9 +1,27 @@
 import { Hono } from "hono";
-import { signup, login } from "../Controllers/authController.js";
+import { signup, login, getUserById,
+  editProfile,
+  changePassword,
+  updateNotifications,
+  getNotifications } from "../Controllers/authController.js";
+import { authMiddleware } from "../Middleware/authMiddleware.js";
 
 const router = new Hono();
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.get("/user/byid",authMiddleware, getUserById);
 
+// Edit profile (username/email)
+router.put("/user/profile",authMiddleware, editProfile);
+
+// Change password
+router.put("/user/password",authMiddleware, changePassword);
+
+// Update notification preferences
+router.put("/user/notifications",authMiddleware, updateNotifications);
+
+// Get notification preferences
+router.get("/user/notifications",authMiddleware, getNotifications);
 export default router;
+
