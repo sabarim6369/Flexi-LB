01dd8876706fc7acfcb4fda42dc4e4c04551d5b7
sabarim6369 <sabarim6369@gmail.com>
2025-09-14 15:56:22 +0530

    added test1 folder

---
+++ Testing2/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+
+app.use(cors());
+
+const app = express();
+const PORT = 4002;
+
+app.get("/", (req, res) => {
+  res.json({
+    server: "Server 2",
+    message: "Hello from Server 2 🎯",
+    time: new Date().toISOString(),
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Server 2 running on http://localhost:${PORT}`);
+});
